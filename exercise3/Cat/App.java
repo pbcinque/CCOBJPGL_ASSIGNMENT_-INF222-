3a5a68e8a2a25831272f08193b321411c126bc04
Bernadette Paguirigan <118649798+pbcinque@users.noreply.github.com>
2023-05-11 21:17:28 +0800

    Update App.java

--- exercise3/Cat/App.java
+++ exercise3/Cat/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
-        // Create object from pet class
+        
         Pet myPet = new Pet();
 
         myPet.meow();
