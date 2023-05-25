89b4dce79531a952f4e731c89f27157558a848cc
Bernadette Paguirigan <118649798+pbcinque@users.noreply.github.com>
2023-05-25 22:01:54 +0800

    Add files via upload

---
+++ Assignment 5/model/Chai.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Chai extends Drink {
+    
+}
---
+++ Assignment 5/model/Drink.java
@@ -0,0 +1,23 @@
+package model;
+
+class Drink {
+
+    private String feel;
+    private String taste;
+
+    public String getFeel() {
+        return this.feel;
+    }
+
+    public void setFeel(String feel) {
+        this.feel = feel;
+    }
+
+    public String getTaste() {
+        return this.taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+}
